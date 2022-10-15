a2af3a5a3f09e72152e2b3dd5b08c52de4224914
yato0 <jdcaramonte159@gmail.com>
2022-10-15 13:21:22 +0800

    fix: home homepulseloader

--- client/src/Components/Home/HomePulseLoader.jsx
+++ client/src/Components/Home/HomePulseLoader.jsx
@@ -3,11 +3,8 @@ import CardPulse from "../CardPulse/CardPulse";
 
 const HomePulseLoader = () => {
   return (
-    <div>
-      <div className="animate-pulse mt-5">
-        <div className="h-6 bg-slate-700 w-48 lg:w-72 rounded"></div>
-      </div>
-      <div className="flex w-full overflow-x-hidden">
+    <div className="mt-10">
+      <div className="flex w-full lg:justify-center  overflow-x-hidden">
         <div className="mr-10">
           <CardPulse />
         </div>
@@ -22,18 +19,9 @@ const HomePulseLoader = () => {
         <div className="mr-10">
           <CardPulse />
         </div>
-        <div className="mr-10">
-          <CardPulse />
-        </div>
       </div>
 
-      <div className="animate-pulse mt-10">
-        <div className="h-6 bg-slate-700 w-48 lg:w-72 rounded"></div>
-      </div>
-      <div className="flex w-full overflow-x-hidden">
-        <div className="mr-10">
-          <CardPulse />
-        </div>
+      <div className="flex w-full lg:justify-center overflow-x-hidden">
         <div className="mr-10">
           <CardPulse />
         </div>
