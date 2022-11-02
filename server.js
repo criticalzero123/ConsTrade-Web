0a5d0f75718ee5dc9219bbd31e60d4166f2e261c
yato0 <jdcaramonte159@gmail.com>
2022-11-02 19:05:13 +0800

    Rerouting when not using SSL

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use("/api/users/", userRoute);
 app.use("/api/transactions/", transactionRoute);
 
 if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    else next();
+  });
+
   app.use("/", express.static("client/build"));
 
   app.get("*", (req, res) => {
