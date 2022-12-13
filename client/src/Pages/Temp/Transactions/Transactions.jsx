190353647454eb5c51d021047358a036901a0a67
yato0 <jdcaramonte159@gmail.com>
2022-12-13 13:52:49 +0800

    Adding loading in fetch api transaction

--- client/src/Pages/Temp/Transactions/Transactions.jsx
+++ client/src/Pages/Temp/Transactions/Transactions.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { RiH1 } from "react-icons/ri";
 
 const Transactions = () => {
   const [transactions, setTransactions] = useState([]);
@@ -13,25 +14,31 @@ const Transactions = () => {
   }, []);
   return (
     <div className="z-50 w-full h-screen absolute top-0 bg-white">
-      <h3 className="text-2xl text-center">
-        Total Transactions: <strong>{transactions.length}</strong>
-      </h3>
-      <ol className="list-inside list-decimal">
-        {transactions.map((transaction, index) => (
-          <li key={index}>
-            <span className="text-sm text-gray-500 mr-2 ">
-              ({transaction.product.preferTrade})
-            </span>
-            {transaction.sellerName} {" -> "} {transaction.buyerName}
-            <span className="text-sm text-gray-500 ml-2 ">
-              || {transaction.product.title}
-            </span>
-            <span className="text-sm text-gray-500 ml-2 ">
-              || Get Wanted: {transaction.getWanted}
-            </span>
-          </li>
-        ))}
-      </ol>
+      {transactions !== 0 ? (
+        <>
+          <h3 className="text-2xl text-center">
+            Total Transactions: <strong>{transactions.length}</strong>
+          </h3>
+          <ol className="list-inside list-decimal">
+            {transactions.map((transaction, index) => (
+              <li key={index}>
+                <span className="text-sm text-gray-500 mr-2 ">
+                  ({transaction.product.preferTrade})
+                </span>
+                {transaction.sellerName} {" -> "} {transaction.buyerName}
+                <span className="text-sm text-gray-500 ml-2 ">
+                  || {transaction.product.title}
+                </span>
+                <span className="text-sm text-gray-500 ml-2 ">
+                  || Get Wanted: {transaction.getWanted}
+                </span>
+              </li>
+            ))}
+          </ol>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
