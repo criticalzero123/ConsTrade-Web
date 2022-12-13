82728925aae8ff3fad5755357fa6a52eec5d328d
yato0 <jdcaramonte159@gmail.com>
2022-12-13 22:32:28 +0800

    Adding review in transaction api

--- client/src/Pages/Temp/AllUser/AllUser.jsx
+++ client/src/Pages/Temp/AllUser/AllUser.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
 const AllUser = () => {
   const [users, setUsers] = useState([]);
   useEffect(() => {
@@ -13,15 +12,16 @@ const AllUser = () => {
   }, []);
 
   return (
-    <div className="h-screen z-50 absolute top-0 bg-white w-full">
+    <div className="h-screen z-50 absolute top-0 bg-white w-full p-5">
       <div className="text-center text-2xl">
         Total User(s): <strong>{users.length}</strong>
       </div>
       <ol className="list-decimal list-inside">
         {users.map((user) => (
-          <li key={user._id} className="capitalize">
-            {user.name}{" "}
-            <span className="text-sm text-gray-500">
+          <li key={user._id} className="capitalize flex place-items-center">
+            {user.name}
+
+            <span className="text-sm text-gray-500 ml-3 mr-1">
               ({user.emailVerified ? "Semi-Verified" : "Not-Verified"})
             </span>
           </li>
--- client/src/Pages/Temp/Transactions/Transactions.jsx
+++ client/src/Pages/Temp/Transactions/Transactions.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { AiFillStar } from "react-icons/ai";
 
 const Transactions = () => {
   const [transactions, setTransactions] = useState([]);
@@ -13,27 +14,44 @@ const Transactions = () => {
   }, []);
 
   return (
-    <div className="z-50 w-full h-screen absolute top-0 bg-white">
+    <div className="z-50 w-full h-screen absolute top-0 bg-white p-5">
       {transactions.length !== 0 ? (
         <>
           <h3 className="text-2xl text-center">
             Total Transactions: <strong>{transactions.length}</strong>
           </h3>
           <ol className="list-inside list-decimal">
-            {transactions.map((transaction, index) => (
-              <li key={index}>
-                <span className="text-sm text-gray-500 mr-2 ">
-                  ({transaction.product.preferTrade})
-                </span>
-                {transaction.sellerName} {" -> "} {transaction.buyerName}
-                <span className="text-sm text-gray-500 ml-2 ">
-                  || {transaction.product.title}
-                </span>
-                <span className="text-sm text-gray-500 ml-2 ">
-                  || Get Wanted: {transaction.getWanted}
-                </span>
-              </li>
-            ))}
+            {transactions.map((transaction, index) => {
+              let _randomNumber = transaction.sellerName.split(" ");
+
+              const random =
+                _randomNumber[0].length > 5 ? 5 : _randomNumber[0].length;
+
+              return (
+                <li key={index} className=" flex place-items-center">
+                  <span className="text-sm text-gray-500 mr-2 ">
+                    ({transaction.product.preferTrade})
+                  </span>
+                  {transaction.sellerName} {" -> "} {transaction.buyerName}
+                  <span className="text-sm text-gray-500 ml-2 ">
+                    || {transaction.product.title}
+                  </span>
+                  <span className="text-sm text-gray-500 ml-2 ">
+                    || Get Wanted: {transaction.getWanted}
+                  </span>
+                  <span className="flex place-items-center">
+                    ||
+                    {[
+                      ...Array(random)
+                        .fill("")
+                        .map((v, index) => (
+                          <AiFillStar key={index} size={17} color={"gold"} />
+                        )),
+                    ]}
+                  </span>
+                </li>
+              );
+            })}
           </ol>
         </>
       ) : (
